9f5ea822c8a8b8fb905b7c86dbdd123f02b46356
HP <safari.mey3am@gmail.com>
2025-11-14 14:43:28 +0330

    add

--- admin/src/components/elements/Checkbox.tsx
+++ admin/src/components/elements/Checkbox.tsx
@@ -14,7 +14,7 @@ function Checkbox({
     <CheckboxPrimitive.Root
       data-slot="checkbox"
       className={cn(
-        "peer border-br data-[state=checked]:bg-primary data-[state=checked]:text-static-w data-[state=checked]:border-primary focus-visible:border-primary focus-visible:ring-primary/20 aria-invalid:ring-red-1/20 aria-invalid:border-red-1 size-4 shrink-0 rounded-[4px] border shadow-xs transition-shadow outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 cursor-pointer",
+        "peer border-br data-[state=checked]:bg-primary data-[state=checked]:text-static-w data-[state=checked]:border-primary data-[state=indeterminate]:bg-primary data-[state=indeterminate]:text-static-w data-[state=indeterminate]:border-primary focus-visible:border-primary focus-visible:ring-primary/20 aria-invalid:ring-red-1/20 aria-invalid:border-red-1 size-4 shrink-0 rounded-[4px] border shadow-xs transition-shadow outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 cursor-pointer",
         className
       )}
       {...props}
